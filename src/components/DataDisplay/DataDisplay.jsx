7b5eab86454a3dfca8769f2140b0dec264d9bf4a
kingkazuma7 <your.nasionasio7@gmail.com>
2023-10-30 12:10:57 +0900

    静的に画像表示まで

--- src/components/DataDisplay/DataDisplay.jsx
+++ src/components/DataDisplay/DataDisplay.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import image1 from '../../assets/images/5bd5a5ff98d12967709d9a95387d60a2342e7235_large.jpeg';
+import image2 from '../../assets/images/644a893108f33b616d32c8085756d627dab9dda4_large.jpeg';
 
 const DataDisplay = () => {
   const data = [
     {
       "name": "朝倉未来",
       "entranceSong": "Battle Scars",
-      "images": ""
+      "image": image1
     },
     {
       "name": "朝倉海",
       "entranceSong": "Forever Young",
-      "images": ""
+      "image": image2
     }
   ];
 
@@ -20,6 +22,7 @@ const DataDisplay = () => {
         <div key={index}>
           <p>{item.name}</p>
           <p>{item.entranceSong}</p>
+          <p><img src={item.image} alt={item.name} /></p>
         </div>
       ))}
     </div>
