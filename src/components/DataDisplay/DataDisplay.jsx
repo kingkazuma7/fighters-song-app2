c58d21de56b289fa01a8f91c729f910ceaf3891e
kingkazuma7 <your.nasionasio7@gmail.com>
2023-10-30 12:19:21 +0900

    画像pathをpublicに移動

--- src/components/DataDisplay/DataDisplay.jsx
+++ src/components/DataDisplay/DataDisplay.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import image1 from '../../assets/images/5bd5a5ff98d12967709d9a95387d60a2342e7235_large.jpeg';
-import image2 from '../../assets/images/644a893108f33b616d32c8085756d627dab9dda4_large.jpeg';
 
 const DataDisplay = () => {
   const data = [
     {
       "name": "朝倉未来",
       "entranceSong": "Battle Scars",
-      "image": image1
+      "image": "/images/5bd5a5ff98d12967709d9a95387d60a2342e7235_large.jpeg"
     },
     {
       "name": "朝倉海",
       "entranceSong": "Forever Young",
-      "image": image2
+      "image": "/images/644a893108f33b616d32c8085756d627dab9dda4_large.jpeg"
     }
   ];
 
@@ -22,7 +20,7 @@ const DataDisplay = () => {
         <div key={index}>
           <p>{item.name}</p>
           <p>{item.entranceSong}</p>
-          <p><img src={item.image} alt={item.name} /></p>
+          <img src={item.image} alt={item.name} width={50} />
         </div>
       ))}
     </div>
