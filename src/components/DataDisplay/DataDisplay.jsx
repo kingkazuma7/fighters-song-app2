9a82dcba115c5dec39c0508ff398adf1856fb93f
kingkazuma7 <your.nasionasio7@gmail.com>
2023-10-31 08:40:13 +0900

    json 分離

--- src/components/DataDisplay/DataDisplay.jsx
+++ src/components/DataDisplay/DataDisplay.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
+import data from "../../common/data.json";
 
 const DataDisplay = () => {
-  const data = [
-    {
-      "name": "朝倉未来",
-      "entranceSong": "Battle Scars",
-      "image": "/images/5bd5a5ff98d12967709d9a95387d60a2342e7235_large.jpeg"
-    },
-    {
-      "name": "朝倉海",
-      "entranceSong": "Forever Young",
-      "image": "/images/644a893108f33b616d32c8085756d627dab9dda4_large.jpeg"
-    }
-  ];
+  
 
   return (
     <div>
