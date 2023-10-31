61ff66237afbef4cf3b2d0e3513540d536bd37ff
kingkazuma7 <your.nasionasio7@gmail.com>
2023-10-31 11:31:27 +0900

    チーム,所属の絞り込み機能

--- src/components/DataDisplay/DataDisplay.jsx
+++ src/components/DataDisplay/DataDisplay.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import data from "../../common/data.json";
 import { Card, CardContent, Typography } from '@mui/material';
 
 const DataDisplay = () => {
+  const [selectedClass, setSelectedClass] = useState(""); // 階級の状態変数
+  const [selectedTeam, setSelectedTeam] = useState(""); // 所属の状態変数
   
+  const filteredData = data.filter(item =>
+    (selectedClass === "" || item.class === selectedClass) &&
+    (selectedTeam === "" || item.team === selectedTeam)
+  );
+  
+  const classes = [...new Set(data.map(item => item.class))];
+  const teams = [...new Set(data.map(item => item.team))];
+  
+  const handleClassChange = (e) => {
+    setSelectedClass(e.target.value);
+    console.log(e.target.value);
+  }
+  const handleTeamChange = (e) => {
+    setSelectedTeam(e.target.value);
+    console.log(e.target.value);
+  }
 
   return (
     <div>
-      {data.map((item, index) => (
+      {/* filter */}
+      <select value={selectedClass} onChange={handleClassChange}>
+        <option value="">階級で絞り込み</option>
+        {classes.map(className => (
+          <option key={className} value={className}>{className}</option>
+        ))}
+      </select>
+      <select value={selectedTeam} onChange={handleTeamChange}>
+        <option value="">チームで絞り込み</option>
+        {teams.map(teamName => (
+          <option key={teamName} value={teamName}>{teamName}</option>
+        ))}
+      </select>
+      
+      {filteredData.map((item, index) => (
         <Card key={index}>
           <CardContent>
             <Typography variant="h5">{item.name}</Typography>
